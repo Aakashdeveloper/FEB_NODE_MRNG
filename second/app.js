b763410c9b6e814bee17df4b25098e6c668abd5f
aakashDeveloper <ahanda205@gmail.com>
2019-02-22 08:55:08 +0530

    adding gitignore

--- second/app.js
+++ second/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 let server = http.createServer((req,res) => {
-    res.write("<h1>Welcome to node</h1>");
+    res.write("<h1>Welcome to node git</h1>");
     res.end()
 })
 
