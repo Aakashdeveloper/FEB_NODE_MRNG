e89dc7174cef1449dfb5bda3b12c444e53cfae65
aakashDeveloper <ahanda205@gmail.com>
2019-02-28 09:04:28 +0530

    a

--- first/app.js
+++ first/app.js
@@ -2,12 +2,25 @@ import express from 'express';
 const app = express();
 const port = '8900';
 
+// Static file path
+app.use(express.static(__dirname+'/public'));
+// Html or rending Path
+app.set('views', './src/views');
+// View engine specification
+app.set('view engine', 'ejs');
+
 app.get('/', (req,res) => {
-    res.send('<h1>Running With Express</h1>');
+    res.render('index',{title:"HomePage",
+                        menu:[{link:'/',name:'Home'}, 
+                               {link:'/movies',name:'Movies'},
+                            {link:'/', name:'Contact'}]});
 })
 
-app.get('/about',(req,res) => {
-    res.send('<h1>This is about page</h1>')
+app.get('/movies',(req,res) => {
+    res.render('movies',{title:'Movies Page', 
+                        menu:[{link:'/',name:'Home'}, 
+                              {link:'/movies',name:'Movies'},
+                             {link:'/', name:'Contact'}]});
 })
 
 app.listen(port, ()=> {
