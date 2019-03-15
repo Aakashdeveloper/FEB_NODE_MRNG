bb6205afb3bc466ba7c939364a07f1d964cde14a
aakashDeveloper <ahanda205@gmail.com>
2019-03-15 08:53:34 +0530

    a

--- first/src/routes/moviesRoute.js
+++ first/src/routes/moviesRoute.js
@@ -2,54 +2,53 @@ import express from 'express';
 const moviesRouter = express.Router();
 const mongodb = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/";
+const dbName = 'acadgild_aug';
+import 'babel-polyfill';
 
 function router(menu) {
   moviesRouter.route('/')
   .get((req,res) => {
-      mongodb.connect(url, (err,db) => {
-        if(err){
-          throw err;
+     (async function mongo(){
+       let client;
+       try{
+         client = await mongodb.connect(url);
+         const db = client.db(dbName);
+         const col = await db.collection('books');
+         const movies = await col.find().toArray();
+          res.render('movies',{
+            title:'Movies Page', 
+            menu, 
+            movies:movies});
         }
-        const dbo = db.db('acadgild_aug');
-        dbo.collection('books').find({}).toArray(
-          (err,data) => {
-            if(err) {
-              throw err;
-            }else{
-              res.render('movies',{
-                          title:'Movies Page', 
-                          menu, 
-                          movies:data});
-            }
-          }
-        );
-      });
+       catch(err){
+         console.log(err);
+       }
+       client.close();
+     }());
       
   });
 
   moviesRouter.route('/:id')
           .get((req,res) => {
               const {id} = req.params;
-              console.log("params form url",id)
-              mongodb.connect(url, (err,db) => {
-                if(err){
-                  throw err;
-                }
-                const dbo = db.db('acadgild_aug');
-                dbo.collection('books').findOne({_id:id}, (err,data) => {
-                  if(err) {
-                    throw err;
-                  }else{
-                    console.log('for 1 movies',data);
-                    var name = data.name;
-                    name = name.charAt(0).toUpperCase()+name.slice(1);
-                    res.render('details',{
-                                title:name, 
-                                menu,
-                                details:data});
-                  }
-                });
-            });    
+          (async function mongo(){
+            let client;
+            try{
+              client = await mongodb.connect(url);
+              const db = client.db(dbName);
+              const col = await db.collection('books');
+              const details = await col.findOne({_id:id})
+              res.render('details',{
+                title:details.name, 
+                menu,
+                details});
+            }
+            catch(err){
+              console.log(err);
+            }
+            client.close();
+          }());
+                
       });
 
   return moviesRouter;
@@ -57,3 +56,5 @@ function router(menu) {
 
 
 module.exports = router;
+
+
